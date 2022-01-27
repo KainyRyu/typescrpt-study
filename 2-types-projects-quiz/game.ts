a589eb0ee5a6add5dd86cc1a6e0cdf1a1aa00463
kainy ryu <kainy2109@gmail.com>
2022-01-27 17:01:46 +0900

    game

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,24 @@
 /**
  * Let's make a game 🕹
  */
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+let position = { x: 0, y: 0 };
+
+function move(direction: Direction) {
+  if (direction === 'up') {
+    position.y = position.y + 1;
+    return position;
+  } else if (direction === 'down') {
+    return { ...position, y: position.y - 1 };
+    console.log('down');
+  } else if (direction === 'left') {
+    return { ...position, x: position.x + 1 };
+    console.log('left');
+  } else if (direction === 'right') {
+    return { ...position, x: position.x - 1 };
+  }
+}
 
 console.log(position); // { x: 0, y: 0}
 move('up');
