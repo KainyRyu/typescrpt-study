26a86b9ea6d0cbda7c3c609bc603f2e8098ae085
kainy ryu <kainy2109@gmail.com>
2022-02-03 23:55:17 +0900

    static member variable

--- 3-oop/3-2-class.ts
+++ 3-oop/3-2-class.ts
@@ -6,20 +6,23 @@
   // class : 서로 관련있는 데이터나 함수들을 한곳에 묶어놓는 기능
   // 청사진을(template) 만드는 기능도 함.
   class CoffeeMaker {
-    BEANS_GRAMM_PER_SHOT: number = 7;
+    // BEANS_GRAMM_PER_SHOT: number = 7;
     // 멤버 변수 => class가 실행 될 때마다 재정의 되어서 메모리 낭비가 됨.
+    static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    // static 을 붙여 class level로 바뀜.
+    // => this.BEANS_GRAMM_PER_SHOT 대신 CoffeeMaker.BEANS_GRAMM_PER_SHOT 사용
 
-    coffeeBeans: number = 0;
+    coffeeBeans: number = 0; // instance (object) level
     constructor(coffeeBeans: number) {
       // class를 가지고 instance를 만들 때 항상 처음에 호출하는 함수
       this.coffeeBeans = coffeeBeans;
     }
 
     makeCoffee(shots: number): CoffeeCup {
-      if (this.coffeeBeans < shots * this.BEANS_GRAMM_PER_SHOT) {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
         throw new Error('Not enough coffee beans!');
       }
-      this.coffeeBeans -= shots * this.BEANS_GRAMM_PER_SHOT;
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
       return {
         shots,
         hasMilk: false
