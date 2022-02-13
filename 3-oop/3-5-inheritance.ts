07766d3c8e392c6484900f8ceb6d27d9433573fd
kainy ryu <kainy2109@gmail.com>
2022-02-13 22:11:21 +0900

    Make latte

---
+++ 3-oop/3-5-inheritance.ts
@@ -0,0 +1,68 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    makeLatte(shots: number, milk: boolean): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    clean() {
+      console.log('cleaning the machine...');
+    }
+
+    grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    preheat(): void {
+      console.log('heating up... 🔥');
+    }
+    steamMilk(): void {
+      console.log('steaming milk..🥛');
+    }
+
+    extract(shots: number, hasMilk?: boolean): CoffeeCup {
+      console.log(`Pulling ${shots} shots...`);
+      return {
+        shots,
+        hasMilk: false
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+
+    makeLatte(shots: number, hasMilk: boolean): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      this.steamMilk();
+      return this.extract(shots);
+    }
+  }
+
+  const order: CoffeeMaker = CoffeeMachine.makeMachine(50);
+
+  order.makeLatte(3, true);
+}
