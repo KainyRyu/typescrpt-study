35cf1044a1dc666dd0ade645c4702fa08c625283
Kainy Ryu <kainy2109@gmail.com>
2022-02-23 20:03:50 +0900

    3-8 decoupling

--- 3-oop/3-8-interface.ts
+++ 3-oop/3-8-interface.ts
@@ -57,8 +57,40 @@
       return this.extract(shots);
     }
   }
+
+  interface MilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup;
+  }
+  interface SugarProvider {
+    addSugar(cup: CoffeeCup): CoffeeCup;
+  }
+
   //싸구려 우유 거품기
-  class CheapMilkSteamer {
+  class CheapMilkSteamer implements MilkFrother {
+    private steamMilk(): void {
+      console.log("Steaming some milk...");
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+  class FancyMilkSteamer implements MilkFrother {
+    private steamMilk(): void {
+      console.log("Steaming some milk...");
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+  class ColdMilkSteamer implements MilkFrother {
     private steamMilk(): void {
       console.log("Steaming some milk...");
     }
@@ -72,9 +104,22 @@
   }
 
   // sugar maker
-  class candySugarMixer {
+  class candySugarMixer implements SugarProvider {
     private getSugar() {
       console.log("Getting some sugar from candy");
+      return true;
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      };
+    }
+  }
+  class SugarMixer implements SugarProvider {
+    private getSugar() {
+      console.log("Getting some sugar from jar");
       return true;
     }
     addSugar(cup: CoffeeCup): CoffeeCup {
@@ -89,7 +134,8 @@
     constructor(
       beans: number,
       public readonly serialNumber: string,
-      private milkFrother: CheapMilkSteamer
+      // private milkFrother: CheapMilkSteamer //class 끼리 커플링하는게아니라
+      private milkFrother: MilkFrother // interface사용해 디커플링
     ) {
       super(beans);
     }
@@ -101,7 +147,7 @@
   }
 
   class SweetCoffeeMaker extends CoffeeMachine {
-    constructor(public beans: number, private sugar: candySugarMixer) {
+    constructor(public beans: number, private sugar: SugarProvider) {
       super(beans);
     }
 
@@ -116,7 +162,7 @@
     constructor(
       public beans: number,
       private sugar: candySugarMixer,
-      private milkFrother: CheapMilkSteamer
+      private milkFrother: MilkFrother
     ) {
       super(beans);
     }
@@ -128,10 +174,36 @@
     }
   }
 
+  // const cheapMilkMaker = new CheapMilkSteamer();
+  // const candySugar = new candySugarMixer();
+  // const sweetMachine = new SweetCoffeeMaker(12, candySugar);
+  // const latteMachine = new CaffeLatteMachine(12, "SS", cheapMilkMaker);
+  // const sweetLatteMachine = new SweetCaffeeLatteMachine(
+  //   6,
+  //   candySugar,
+  //   cheapMilkMaker
+  // );
+  // NOT PRODUCTIVE AT ALL
+
+  // 디커플링의 원칙!
+  // !!! class들 사이에 서로 상호작용하는 경우에는
+  // class 자신을 노출하는게 아니라 계약서를 통해 의사소통 해야 함 = interface
+
+  // Milk
   const cheapMilkMaker = new CheapMilkSteamer();
+  const fancyMilkMaker = new FancyMilkSteamer();
+  const coldMilkMaker = new ColdMilkSteamer();
+
+  //Sugar
   const candySugar = new candySugarMixer();
-  const sweetMachine = new SweetCoffeeMaker(12, candySugar);
+  const sugar = new SugarMixer();
+
+  //
+  const sweetCandyMachine = new SweetCoffeeMaker(12, candySugar);
+  const sweetMachine = new SweetCoffeeMaker(12, sugar);
+  // 원하는 용도에 따라 재료 아무거나 넣어서 원하는대로 만들 수 있어
   const latteMachine = new CaffeLatteMachine(12, "SS", cheapMilkMaker);
+  const coldLatteMachine = new CaffeLatteMachine(12, "SS", coldMilkMaker);
   const sweetLatteMachine = new SweetCaffeeLatteMachine(
     6,
     candySugar,
